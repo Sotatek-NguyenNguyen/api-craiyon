fe118bfbb82025dedd622dd9c4bf6c3aa89e71d5
nguyen <nguyen.nguyen@sotatek.com>
2023-05-23 17:56:51 +0700

    a

--- src/common/guard/auth.guard.ts
+++ src/common/guard/auth.guard.ts
@@ -7,6 +7,11 @@ export class AuthGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
+    console.log("request.headers?.appid" , request.headers?.appid);
+    console.log("process.env.APP_ID" , process.env.APP_ID);
+    if (request.headers?.appid == undefined || request.headers?.webservice_api_key == undefined) {
+      return false;
+    }
     return request.headers?.appid === process.env.APP_ID && request.headers?.webservice_api_key === process.env.WEBSERVICE_API_KEY;
   }
 
